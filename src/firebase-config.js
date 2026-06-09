cf4333f2e7a2dcfbe2b55b84f13466b5064eccef
Oluwatimileyin Emmanuel Ogundere <emekaogundele@gmail.com>
2026-06-09 22:02:06 +0100

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,8 @@
-// Replace with your Firebase project configuration
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCyeeSfRJptjqipTlWv8rl2dWhvKJmyDF8",
+  authDomain: "content-i.firebaseapp.com",
+  projectId: "content-i",
+  storageBucket: "content-i.firebasestorage.app",
+  messagingSenderId: "934708195485",
+  appId: "1:934708195485:web:ecab357491c6d201ebe474"
 };
